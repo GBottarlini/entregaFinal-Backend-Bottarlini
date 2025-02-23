ac45c6ed045581b4e747b02fb735ac2073949748
Genaro Bottarlini <bottarlini.99@outlook.com>
2025-02-23 12:00:30 -0300

    implementacion final y mejora de interfaz usuarios

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import dotenv from 'dotenv';
+dotenv.config();
 import express from 'express'
 import path from 'path'
 import { __dirname } from './path.js'
@@ -7,59 +9,49 @@ import {create} from 'express-handlebars'
 import passport from 'passport'
 import initalizatePassport from './config/passport.config.js'
 import MongoStore from 'connect-mongo'
-import sessionRouter from './routes/sessions.routes.js'
-import productRouter from './routes/products.routes.js'
-import cartRouter from './routes/carts.routes.js'
+import indexRouter from './routes/index.routes.js'
 import mongoose from 'mongoose'
 import cors from 'cors'
-import bodyParser from "body-parser"; 
-import { userInfo } from 'os'
+
+
 
 const app = express()
 const PORT = 5000
-const hbs = create()
+const hbs = create({
+    defaultLayout: 'main',
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    partialsDir: path.join(__dirname, 'views/partials'),
+})
 app.use(cors())
 app.use(express.json())
-app.use(cookieParser())
-app.use(express.urlencoded({ extended: true }));
-app.use(
-    session({
-        store: MongoStore.create({
-            mongoUrl: "mongodb+srv://bottarlini99:dKYARPesZY2KkBPW@coder.ucsnn.mongodb.net/?retryWrites=true&w=majority&appName=coder", // URL de MongoDB
-            mongoOptions: {}, // Opciones adicionales de MongoDB
-            ttl: 15 * 60, // Tiempo de vida de la sesión en segundos (15 minutos)
-        }),
-        secret: "SessionSecret", // Clave secreta para firmar la sesión
-        resave: false, // Evita guardar la sesión si no ha cambiado
-        saveUninitialized: false, // Evita guardar sesiones no inicializadas
-        cookie: {
-            maxAge: 15 * 60 * 1000, // Tiempo de vida de la cookie en milisegundos (15 minutos)
-            httpOnly: true, // La cookie solo es accesible desde el servidor
-            secure: false, // Cambiar a true en producción con HTTPS
-        },
-    })
-);  
+app.use(cookieParser(process.env.SECRET_COOKIE)) 
+const mongoUrl = process.env.URL_MONGO; 
+app.use(session({
+    store: MongoStore.create({
+        mongoUrl: mongoUrl,
+        mongoOptions: {},
+        ttl: 15000000000
+    }),
+    secret: process.env.SECRET_SESSION,
+    resave: true,
+    saveUninitialized: true
+}))
 
-mongoose.connect("mongodb+srv://bottarlini99:dKYARPesZY2KkBPW@coder.ucsnn.mongodb.net/?retryWrites=true&w=majority&appName=coder")
+mongoose.connect(mongoUrl)
 .then(() => console.log("DB is connected"))
 .catch((e) => console.log("Error al conectarme a DB:", e))
 
 initalizatePassport()
 app.use(passport.initialize())
 app.use(passport.session())
-app.engine("handlebars", create({ defaultLayout: "main" }).engine);
+app.engine('handlebars', hbs.engine)
 app.set('view engine', 'handlebars')
-app.set('views', path.join(__dirname, 'views')) 
-
+app.set('views', path.join(__dirname, 'views')) //Concateno evitando erroes de / o \
+app.use(express.static(path.join(__dirname, "public")))
 //Rutas
-app.use('/public', express.static(__dirname + '/public')) 
-app.use('/api/sessions', sessionRouter)
-app.use('/api/products', productRouter)
-app.use('/api/carts', cartRouter)
+app.use('/', indexRouter)
 
-app.get('/', (req,res) => {
-    res.status(200).send("Hola desde Inicio")
-})
+
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
 })
--- src/routes/sessions.routes.js
+++ src/routes/sessions.routes.js
@@ -8,16 +8,10 @@ const sessionRouter = Router()
 
 sessionRouter.get('/viewlogin', viewLogin)
 sessionRouter.get('/viewregister', viewRegister)
-sessionRouter.post(
-  "/login",
-  passport.authenticate("login", { failureRedirect: "/login" }), // Usar estrategia "login"
-  (req, res) => {
-      res.redirect("/"); // Redirige al inicio después del login
-  }
-);
+sessionRouter.post('/login', passport.authenticate('login'), login)
 sessionRouter.post('/register', passport.authenticate('register'), register)
 sessionRouter.get('/github', passport.authenticate('github', {scope:['user:email']}), async (req,res) => {})
 sessionRouter.get('/githubcallback', passport.authenticate('github', {failureRedirect:'/login'}), githubLogin)
-sessionRouter.get("/current", passport.authenticate("current", { session: false }), async (req, res) => { res.send(req.user);});
+sessionRouter.get('/current', passport.authenticate('jwt'), authorization("user"), async (req,res) => res.send(req.user))
 
 export default sessionRouter
